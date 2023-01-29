1f018c9c19a65e75444c484f8ab8b992ce9b360a
Github CLI <githubcli@github.com>
2023-01-28 23:16:36 -0300

    missing fix

--- github-cli.sh
+++ github-cli.sh
@@ -19,6 +19,7 @@ create_release(){
     "prerelease": true
   }' | sed -r 's/[\]{2}/\\/g')
   (curl -s -X POST -w '%{stderr}%{http_code}\n%{stdout}\n' \
+      -H "Authorization: token $${REPO_TOKEN}" \
     "https://api.github.com/repos/${USERNAME}/${REPOSITORY}/releases?access_token=$REPO_TOKEN" \
     --data "$PAYLOAD" |\
     tee -a /dev/stderr | jq -r '.id') 2> /tmp/stderr 1> /tmp/stdout
